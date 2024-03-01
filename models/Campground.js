9b6bc0bb818aafd4eea4a5a837ecefb7b4bbb76c
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-03-01 22:02:56 +0700

    campground.js finish

---
+++ models/Campground.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const CampgroundSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please add a name'],
+        unique: true,
+        trim:true,
+        maxlenght:[50,'Name can not be more than 50 characters']
+        },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+        },
+    tel:{
+        type: String,
+        match:[
+            /^\+[1-9]\d{1,14}$/,
+            'Please add a valid telephone number'
+        ]
+        },
+        
+    
+}, {
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+});
+
+
+//Cascade delete bookings when a campground is deleted
+CampgroundSchema.pre('deleteOne',{document:true , query:false},async function(next) {
+    console.log(`Bookings being removed from campground ${this._id}`);
+    await this.model('Booking').deleteMany({campground: this._id});
+    next();
+})
+
+//Reserve populate with virtuals
+CampgroundSchema.virtual('bookings',{
+    ref:'Booking',
+    localField:'_id',
+    foreignField:'campground',
+    justOne:false
+});
+
+module.exports = mongoose.model('Campground',CampgroundSchema);
