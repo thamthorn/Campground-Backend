8a9ee42e4a2957076f3159ab91a8bda2505380f2
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-03-01 21:42:15 +0700

    booking model

---
+++ models/Booking.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema({
+    apptDate: {
+        type : Date,
+        required : true
+    },
+    user: {
+        type:mongoose.Schema.ObjectId,
+        
+        ref: 'User',
+        required : true
+    },
+    campground: {
+        type:mongoose.Schema.ObjectId,
+        
+        ref : 'Campground',
+        required : true
+    },
+    createdAt: {
+        type : Date,
+        default : Date.now
+    }
+});
+
+module.exports = mongoose.model('Booking',BookingSchema);
