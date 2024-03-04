2e93811489158760b12968b1286bc3393a30b58f
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-03-04 15:10:50 +0700

    add model/coupon.js

---
+++ models/Coupon.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const CouponSchema = new mongoose.Schema({
+    discount: {
+        type:String,
+        required : true
+    },
+    detail: {
+        type: String
+    },
+    expiredDate: {
+        type : Date,
+        //30 days expired
+        default : Date.now + (30 * 24 * 60 * 60 * 1000)
+    }
+});
+
+module.exports = mongoose.model('Coupon',CouponSchema);
