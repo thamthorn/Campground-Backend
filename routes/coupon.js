7420dbd5847880058e433b594d1b38266f590276
Watthana Bunyarud <topten.watthana@gmail.com>
2024-03-04 15:13:46 +0700

    routes coupon

---
+++ routes/coupon.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {getCoupons,getCoupon,createCoupon,updateCoupon,deleteCoupon} = require('../controllers/coupon');
+
+const router = express.Router({mergeParams:true});
+
+const {protect,authorize} = require('../middleware/auth');
+
+router.route('/')
+    .get(protect,authorize('vip','admin'),getCoupons)
+    .post(protect,authorize('admin'),createCoupon);
+
+
+
+
+
+router.route('/:id')
+    .get(protect,authorize('vip','admin'),getCoupon)
+    .put(protect,authorize('admin'),updateCoupon)
+    .delete(protect,authorize('admin'),deleteCoupon);
+
+module.exports = router
